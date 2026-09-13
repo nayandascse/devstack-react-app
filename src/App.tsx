b52d0756dafae77d540dee14d644bfbf08a9db61
nayandascse <nayan.cmt@gmail.com>
2026-09-13 22:28:52 +0600

    app.tsx  change technology.json commit

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 useEffect(() => {
   const loadTechnologies = async () => {
     try {
-      const response = await fetch("/public/technologies.json");
+      const response = await fetch("/technologies.json");
 
       if (!response.ok) {
         throw new Error("Failed to load technologies");
